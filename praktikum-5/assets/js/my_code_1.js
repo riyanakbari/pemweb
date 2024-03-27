4279d16bd517dcfe3ef6ef12fdd4b57e3ec578e6
riyanakbari <rynakbri@gmail.com>
2024-03-27 21:12:11 +0700

    praktikum 5

---
+++ praktikum-5/assets/js/my_code_1.js
@@ -0,0 +1,6 @@
+document
+        .getElementById("myButton")
+        .addEventListener(
+          "click",
+          () => (document.getElementById("para").innerHTML = "Kalimat ini telah berubah ketika diclick.")
+        );
---
+++ praktikum-5/assets/js/tugas_praktikum.js
@@ -0,0 +1,8 @@
+document
+        .getElementById("myButton")
+        .addEventListener(
+          "click",
+          () => (document.getElementById("para").innerHTML = "Kalimat ini telah berubah ketika diclick.")
+        );
+
+        alert("Selamat Menunaikan Ibadah Puasa")
